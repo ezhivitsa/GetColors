23af0156b7a8232332e43dd5fb90215dffa320df
Evgeny Zhivitsa <j.zhivitsa@gmail.com>
2016-01-23 14:13:49 +0300

    r

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var reg = /class=\"color-description\"\>\s.+\<strong\>(.*)\<\/strong\>/;
 
 var result = {};
 var doneNum = 0;
-var start = 164*16*16*16*16;
-var numReq = 168*16*16*16*16;
+var start = 168*16*16*16*16;
+var numReq = 172*16*16*16*16;
 var doParallel = 100;
 
 function getColor(color) {
